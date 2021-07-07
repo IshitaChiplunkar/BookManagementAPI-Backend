57c0abad5d66a22bc09811c7648f8df69d3c59e1
unknown <20ishita@gmail.com>
2021-07-08 00:25:17 +0530

    Microservices with express Router

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 require("dotenv").config();
 
+
 // Framework
 const express=require("express");
 const mongoose=require("mongoose");
+
 
 // Microservices Routes
 const Books=require("./API/Book");
 const Authors=require("./API/Author");
 const Publications=require("./API/Publication");
 
+
 // Initializing express
 const shapeAI= express();
 
+
 // Configurations
 shapeAI.use(express.json());
+
 
 //Establish database connection
 mongoose
@@ -25,9 +30,11 @@ mongoose
     }
 ).then(()=>console.log("Connection extablished!!!"));
 
+
 //Initializing Microservices
 shapeAI.use("/book",Books);
 shapeAI.use("/author",Authors);
 shapeAI.use("/publication",Publications);
+
 
 shapeAI.listen(3000,() => console.log("Server running!!"));
